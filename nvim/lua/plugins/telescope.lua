ef1cd129c9d45e7994d519df74ca3d85763a942b
Tin <tin@olli-ai.com>
2025-11-25 17:09:11 +0700

    add smart history to telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,16 +2,30 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
 	dependencies = {
+		"kkharji/sqlite.lua",
 		"nvim-lua/plenary.nvim",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "make",
 		},
 		"nvim-telescope/telescope-live-grep-args.nvim",
+		"nvim-telescope/telescope-smart-history.nvim",
 	},
 	config = function()
 		local telescope = require("telescope")
 		telescope.setup({
+			defaults = {
+				history = {
+					path = "~/.local/share/nvim/databases/telescope_history.sqlite3",
+					limit = 100,
+				},
+				mappings = {
+					i = {
+						["<C-Down>"] = require("telescope.actions").cycle_history_next,
+						["<C-Up>"] = require("telescope.actions").cycle_history_prev,
+					},
+				},
+			},
 			pickers = {
 				oldfiles = {
 					cwd_only = true,
@@ -20,5 +34,6 @@ return {
 		})
 		telescope.load_extension("fzf")
 		telescope.load_extension("live_grep_args")
+		telescope.load_extension("smart_history")
 	end,
 }
