2897979f91763a8cdd429ff8b13cdeb69cd2e492
Tin <tin@olli-ai.com>
2026-06-12 11:30:22 +0700

    feat(gitsigns): add hunk navigation and action keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,3 +3,9 @@ vim.pack.add {
 }
 
 require("gitsigns").setup {}
+
+vim.keymap.set("n", "]c", "<cmd>Gitsigns nav_hunk next<cr>", { desc = "Next git hunk" })
+vim.keymap.set("n", "[c", "<cmd>Gitsigns nav_hunk prev<cr>", { desc = "Previous git hunk" })
+vim.keymap.set("n", "<leader>hs", "<cmd>Gitsigns stage_hunk<cr>", { desc = "Stage hunk" })
+vim.keymap.set("n", "<leader>hr", "<cmd>Gitsigns reset_hunk<cr>", { desc = "Reset hunk" })
+vim.keymap.set("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<cr>", { desc = "Preview hunk" })
