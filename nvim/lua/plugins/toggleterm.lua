246c9d129d45db39cb80709b81d9a47bf211b0e6
Tin <tin@olli-ai.com>
2026-06-12 10:17:18 +0700

    feat(toggleterm): install via vim.pack

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,38 +1,36 @@
-return {
-	"tinnguyen2909/toggleterm.nvim",
-	version = "*",
-	config = function()
-		require("toggleterm").setup({
-			size = 20,
-			open_mapping = [[<c-\>]],
-			hide_numbers = true,
-			shade_filetypes = {},
-			shade_terminals = true,
-			shading_factor = 2,
-			start_in_insert = true,
-			insert_mappings = true,
-			persist_size = true,
-			direction = "float",
-			close_on_exit = true,
-			shell = vim.o.shell,
-			float_opts = {
-				border = "curved",
-				winblend = 0,
-				highlights = {
-					border = "Normal",
-					background = "Normal",
-				},
-			},
-		})
-
-		-- Function to set terminal keymaps
-		function _G.set_terminal_keymaps()
-			local opts = { buffer = 0 }
-			vim.keymap.set("n", "<esc>", "<cmd>close<cr>", opts)
-			vim.keymap.set("n", "q", "<cmd>close<cr>", opts)
-		end
-
-		-- Apply terminal keymaps when terminal opens
-		vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
-	end,
+vim.pack.add {
+  { src = "https://github.com/tintty1/toggleterm.nvim" },
 }
+
+require("toggleterm").setup {
+  size = 20,
+  open_mapping = [[<c-\>]],
+  hide_numbers = true,
+  shade_filetypes = {},
+  shade_terminals = true,
+  shading_factor = 2,
+  start_in_insert = true,
+  insert_mappings = true,
+  persist_size = true,
+  direction = "float",
+  close_on_exit = true,
+  shell = vim.o.shell,
+  float_opts = {
+    border = "curved",
+    winblend = 0,
+    highlights = {
+      border = "Normal",
+      background = "Normal",
+    },
+  },
+}
+
+-- Function to set terminal keymaps
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set("n", "<esc>", "<cmd>close<cr>", opts)
+  vim.keymap.set("n", "q", "<cmd>close<cr>", opts)
+end
+
+-- Apply terminal keymaps when terminal opens
+vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
