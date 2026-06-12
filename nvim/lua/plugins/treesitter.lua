892be5434f09deacf9ed08980ac000cdf2149901
Tin <tin@olli-ai.com>
2026-06-12 10:41:36 +0700

    feat(treesitter): install via vim.pack

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,35 @@
-return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	lazy = false,
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"python",
-				"markdown",
-				"lua",
-				"javascript",
-				"yaml",
-				"typescript",
-				"html",
-				"css",
-				"jsonc",
-				"tsx",
-			},
-			highlight = {
-				enable = true,
-			},
-		})
-	end,
+vim.pack.add {
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 }
+
+local parsers = {
+  "python",
+  "markdown",
+  "lua",
+  "javascript",
+  "yaml",
+  "typescript",
+  "html",
+  "css",
+  "jsonc",
+  "tsx",
+}
+
+-- Install missing parsers (async, no-op if already installed).
+require("nvim-treesitter").install(parsers)
+
+-- Keep parsers in sync with the plugin whenever it's updated.
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(ev)
+    if ev.data.spec.name == "nvim-treesitter" and ev.data.kind == "update" then
+      vim.cmd("TSUpdate")
+    end
+  end,
+})
+
+-- Enable treesitter highlighting for any filetype that has an installed parser.
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
