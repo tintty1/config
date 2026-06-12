5cdf407029af583571e4d3101e50b0a90f1929db
Tin <tin@olli-ai.com>
2026-06-12 13:37:41 +0700

    feat(fzf-lua): install via vim.pack with find keymaps

---
+++ nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,29 @@
+vim.pack.add {
+  { src = "https://github.com/ibhagwan/fzf-lua" },
+}
+
+local fzf = require("fzf-lua")
+
+fzf.setup {
+  oldfiles = {
+    cwd_only = true,
+  },
+  buffers = {
+    sort_lastused = true,
+  },
+}
+
+local function map(lhs, rhs, desc)
+  vim.keymap.set("n", lhs, rhs, { desc = desc })
+end
+
+map("<leader><Space>", fzf.builtin, "Fzf builtin pickers")
+map("<leader>ff", fzf.files, "Find files")
+map("<leader>fg", fzf.live_grep, "Live grep")
+map("<leader>fb", fzf.buffers, "Find buffers")
+map("<leader>fh", fzf.helptags, "Help tags")
+map("<leader>fr", fzf.oldfiles, "Recent files")
+map("<leader>fc", fzf.grep_cword, "Find word under cursor")
+map("<leader>fs", fzf.lsp_document_symbols, "Document symbols")
+map("<leader>fS", fzf.lsp_workspace_symbols, "Workspace symbols")
+map("<leader>fd", fzf.diagnostics_document, "Diagnostics")
