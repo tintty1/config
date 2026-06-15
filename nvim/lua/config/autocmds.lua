2170badb234c09e86a97cea18f5e85872f334abb
Tin <tin@olli-ai.com>
2026-06-15 11:50:08 +0700

    remove unused files

--- nvim/lua/config/autocmds.lua
+++
@@ -1,5 +0,0 @@
--- Auto-check for file changes when gaining focus, entering buffer, or after idle
-vim.api.nvim_create_autocmd({"FocusGained", "BufEnter", "CursorHold"}, {
-  pattern = "*",
-  command = "checktime"
-})
--- nvim/lua/plugins/nightfox.lua
+++
@@ -1,5 +0,0 @@
-return {
-	"EdenEast/nightfox.nvim",
-	lazy = false,
-	priority = 1000,
-}
