324e36b91a7b4200b31e6b67d8c132c66f4f513c
Tin <tin@olli-ai.com>
2026-06-19 16:53:08 +0700

    nvim: hide list/number chrome in terminal buffers
    Terminal windows inherited the global list/listchars and number column, so
    shells showed trailing-space markers etc. Turn list, number, and
    relativenumber off on TermOpen.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -3,3 +3,14 @@ vim.api.nvim_create_autocmd({"FocusGained", "BufEnter", "CursorHold"}, {
   pattern = "*",
   command = "checktime"
 })
+
+-- Terminal windows inherit the global editor chrome (list/listchars, number column),
+-- which looks wrong for a shell/tmux buffer. Turn it off for terminals.
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.list = false
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+  end,
+})
