ec2043aa31766900ab6db17a3cb54e2b1012480a
Tin <tin@olli-ai.com>
2026-06-22 16:23:26 +0700

    nvim: stop dimming unreachable/unnecessary code

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -6,4 +6,14 @@ require("catppuccin").setup {
   flavour = "mocha",
 }
 
+-- Don't grey out "unnecessary"/unreachable code (e.g. pyright's false-positive
+-- unreachable reports). Fully clearing the group lets the underlying syntax
+-- highlight show through; pyright's underline/sign still appears.
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "catppuccin*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", {})
+  end,
+})
+
 vim.cmd.colorscheme "catppuccin-mocha"
